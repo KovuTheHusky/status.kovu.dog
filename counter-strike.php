3fa52daf92a08098ad584e7c43b48a9b880ff216
KovuTheHusky <kovuthehusky@gmail.com>
2021-02-08 20:40:14 -0500

    Add urls for Counter-Strike players

--- counter-strike.php
+++ counter-strike.php
@@ -43,20 +43,22 @@ while (true) {
             for ($time = time(); $time == time(); usleep(1000));
             $json->Info = $query->GetInfo();
             $status = $query->Rcon('status');
-            preg_match_all('/"(.*)" STEAM_1:([0-9]+):([0-9]+)/i', $status, $players, PREG_SET_ORDER);
+            preg_match_all('/STEAM_1:([0-9]+):([0-9]+)/i', $status, $players, PREG_SET_ORDER);
             $arr = array();
             foreach ($players as $player) {
-                $id = (($player[3] * 2) + $player[2]) + 76561197960265728;
-                $name = $player[1];
+                $id = (($player[2] * 2) + $player[1]) + 76561197960265728;
 
                 $req = json_decode(file_get_contents('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' . COUNTERSTRIKE_SECRET . '&steamids=' . $id));
 
                 $avatar = $req->response->players[0]->avatarfull;
+                $name = $req->response->players[0]->personaname;
+                $url = $req->response->players[0]->profileurl;
 
                 $arr[] = array(
                     'id' => $id,
                     'name' => $name,
-                    'avatar' => $avatar
+                    'avatar' => $avatar,
+                    'url' => $url
                 );
             }
             //preg_match_all('/"(.*)" BOT/i', $status, $bots, PREG_SET_ORDER);
