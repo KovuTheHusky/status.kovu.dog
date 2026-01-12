ae9d8c5737361d5e87a476cbfcbf45efa1f24a35
KovuTheHusky <kovuthehusky@gmail.com>
2026-01-12 02:03:05 -0500

    Update minecraft script

--- minecraft.php
+++ minecraft.php
@@ -4,9 +4,13 @@ if (php_sapi_name() != 'cli') {
     exit;
 }
 
-declare(ticks = 1);
+declare(ticks=1);
 
-function shutdown() {
+function shutdown()
+{
+    if (file_exists('minecraft.txt')) {
+        unlink('minecraft.txt');
+    }
     file_put_contents('minecraft.json', '', LOCK_EX);
     exit;
 }
@@ -25,9 +29,7 @@ use xPaw\MinecraftQueryException;
 use xPaw\SourceQuery\SourceQuery;
 
 require __DIR__ . '/configuration.php';
-require __DIR__ . '/node_modules/PHP-Minecraft-Query/src/MinecraftQuery.php';
-require __DIR__ . '/node_modules/PHP-Minecraft-Query/src/MinecraftQueryException.php';
-require __DIR__ . '/node_modules/PHP-Source-Query/SourceQuery/bootstrap.php';
+require __DIR__ . '/vendor/autoload.php';
 
 if (file_exists('minecraft.json') && filesize('minecraft.json') > 0) {
     $json = json_decode(file_get_contents('minecraft.json'));
@@ -63,27 +65,63 @@ while (true) {
             if ($time >= 18000) {
                 ++$day;
             }
-            $json->Day = $day;
-            $json->Time = $time;
-            $weather = json_decode(file_get_contents('https://minecraft.kovu.dog/standalone/MySQL_update.php?world=world'));
-            if ($weather->hasStorm) {
-                if ($weather->isThundering) {
-                    $weather = 'thunder';
-                } else {
-                    $weather = 'rain';
-                }
+            $json->Day = (int) $day;
+            $json->Time = (int) $time;
+            $raining = explode(' ', $rcon->Rcon('execute if predicate [{"condition":"minecraft:weather_check","raining":true}]'))[1] == 'passed';
+            $thundering = explode(' ', $rcon->Rcon('execute if predicate [{"condition":"minecraft:weather_check","thundering":true}]'))[1] == 'passed';
+            if ($thundering) {
+                $weather = 'thunder';
+            } else if ($raining) {
+                $weather = 'rain';
             } else {
                 $weather = 'clear';
             }
             $json->Weather = $weather;
+            $moonIndex = $day % 8;
+            switch ($moonIndex) {
+                case 0:
+                    $moonPhase = 'full_moon';
+                    break;
+                case 1:
+                    $moonPhase = 'waning_gibbous';
+                    break;
+                case 2:
+                    $moonPhase = 'last_quarter';
+                    break;
+                case 3:
+                    $moonPhase = 'waning_crescent';
+                    break;
+                case 4:
+                    $moonPhase = 'new_moon';
+                    break;
+                case 5:
+                    $moonPhase = 'waxing_crescent';
+                    break;
+                case 6:
+                    $moonPhase = 'first_quarter';
+                    break;
+                case 7:
+                    $moonPhase = 'waxing_gibbous';
+                    break;
+            }
+            $json->Moon = $moonPhase;
             $lm = explode(PHP_EOL, $rcon->Rcon('lm'));
-            $json->Tickrate[] = (double) explode(' ', $lm[0])[2];
+            $json->Tickrate[] = (float) explode(' ', $lm[0])[2];
             $memory = explode(' ', $lm[1])[3];
-            $memory = (double) substr($memory, 1, strlen($memory) - 3);
+            $memory = (float) substr($memory, 1, strlen($memory) - 3);
             $json->Memory[] = $memory;
+            $mspt = explode('/', explode(' ', preg_replace('/\xA7[0-9A-FK-OR]/i', '', explode(PHP_EOL, $rcon->Rcon('mspt'))[1]))[2]);
+            $json->Ticktime = new stdClass;
+            $json->Ticktime->Average = (float) preg_replace('/[^0-9.]/', '', $mspt[0]);
+            $json->Ticktime->Minimum = (float) preg_replace('/[^0-9.]/', '', $mspt[1]);
+            $json->Ticktime->Maximum = (float) preg_replace('/[^0-9.]/', '', $mspt[2]);
+            file_put_contents('minecraft.txt', 1, LOCK_EX);
             file_put_contents('minecraft.json', json_encode($json), LOCK_EX);
         }
     } catch (Exception $e) {
+        if (file_exists('minecraft.txt')) {
+            unlink('minecraft.txt');
+        }
         file_put_contents('minecraft.json', '', LOCK_EX);
         file_put_contents('minecraft.log', '[' . date('d-M-y H:i:s T') . '] ' . $e . PHP_EOL, FILE_APPEND | LOCK_EX);
     }
